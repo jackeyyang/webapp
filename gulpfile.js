be00dc3423046c14e780b7ff8972c0521c604812
jackeyyang <jackeyyang@coderedfour.com>
2016-09-20 16:15:38 +0800

    gulp-compass

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,26 @@
  * Created by jk on 2016/8/22 0022.
  */
 
+var paths = {
+    scss: './assets/sass/*.scss',
+    css: './assets/css/'
+};
+
 var gulp = require('gulp')
     compass = require('gulp-compass');
 
 gulp.task('css',function () {
-    gulp.src('./assets/sass/*.scss')
+    gulp.src(paths.scss)
         .pipe(compass({
-            css: './assets/css/',
+            css: paths.css,
             sass: './assets/sass/'
         }))
-        .pipe(gulp.dest('./assets/css/'));
+        .pipe(gulp.dest(paths.css));
 });
 
-gulp.task('default',['css']);
+gulp.task('watch',function () {
+    gulp.watch(paths.scss,['css']);
+});
+
+
+gulp.task('default',['css','watch']);
